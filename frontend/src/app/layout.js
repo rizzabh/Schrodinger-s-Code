369fe624f16a02df0e2f8d62b6fa7a1967f4b2ad
rizzabh <pandeyrishabh966@gmail.com>
2025-02-22 03:53:26 +0530

    sonner

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import WalletContextProvider from "./components/WalletProvider";
+import { Toaster } from "sonner";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <WalletContextProvider>
-          
+        <Toaster position="top-center" richColors />
           {children}
         </WalletContextProvider>{" "}
       </body>
