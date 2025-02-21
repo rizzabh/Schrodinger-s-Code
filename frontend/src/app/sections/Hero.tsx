ef982960fb43610abd24697a1a123e5a4e448933
rizzabh <pandeyrishabh966@gmail.com>
2025-02-21 13:58:33 +0530

    first

---
+++ frontend/src/app/sections/Hero.tsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import Logo from "../components/svg/Logo";
+import HeroBackground from "../components/background/HeroBackground";
+import React from "react";
+
+const Hero = () => {
+    return (
+        <motion.div
+            className="relative z-10 flex h-[100vh] w-full justify-center"
+            id="home"
+            initial="initial"
+            animate="animate"
+        >
+            <HeroBackground />
+            <div className="mt-10 flex flex-col items-center justify-center sm:mt-0">
+                <div
+                    className={`relative flex flex-col items-center justify-center pointer-events-none`}
+                >
+                    <Logo width={100} height={100} />
+                </div>
+            </div>
+        </motion.div>
+    );
+};
+
+export default Hero;
